aa8dcc896f02763fff17e0b2edcb799ff83da93c
Connor <connor@pop-os.localdomain>
2021-04-02 22:46:12 -0400

    V2 support start

--- src/index.js
+++ src/index.js
@@ -18,6 +18,32 @@ let root = document.createElement('div');
 root.id = 'root';
 document.body.appendChild(root);
 
+// en/decryption will fail, but signatures will work
+import { AccountSystem, MetadataAccess } from "../ts-client-library/packages/account-system/src/index.js"
+import { WebAccountMiddleware, WebNetworkMiddleware } from "../ts-client-library/packages/middleware-web/src/index.js"
+import { hexToBytes } from "../ts-client-library/packages/util/src/hex.js"
+
+const storageNode = "https://beta-broker.opacitynodes.com:3000"
+
+const run = async () => {
+  const handle = hexToBytes("922323580916c93c25eef655691f2c672c1b46647aaa55be2e42013dd88e8ed434534b4a6cb137a03167bb4d72686b98e1274a592f71b9c75df1d2fbf22abd6f")
+
+  const cryptoMiddleware = new WebAccountMiddleware({ asymmetricKey: handle })
+  const netMiddleware = new WebNetworkMiddleware()
+
+  const metadataAccess = new MetadataAccess({
+    net: netMiddleware,
+    crypto: cryptoMiddleware,
+    metadataNode: storageNode,
+  })
+  const accountSystem = new AccountSystem({ metadataAccess })
+
+  console.log(accountSystem)
+}
+
+run()
+
+
 // Now we can render our application into it
 render(
   <Router>
